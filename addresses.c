2fb44f505d89e7307f0b44576fe6899f15973b66
Olaf Kirch <okir@suse.com>
2025-09-24 13:35:22 +0200

    Wake up, it's 2025
    Signed-off-by: Olaf Kirch <okir@suse.com>

--- addresses.c
+++ addresses.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2023 Olaf Kirch <okir@suse.com>
+ * Copyright (C) 2025 Olaf Kirch <okir@suse.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
